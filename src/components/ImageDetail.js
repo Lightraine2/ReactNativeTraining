0a8c986e206f1222264b0642a03da19dbdbf901d
Laurence Dale <laurencedale1986@gmail.com>
2020-03-29 10:19:42 +0100

    38 - state in components

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = (props) => {
+const ImageDetail = ({imageSource, title, imageScore }) => {
     
-return <Text>{props.title}</Text>;
+    return (
+     <View>
+        <Image source={imageSource}/>
+        <Text>{title}</Text>
+        <Text>Image Score {imageScore}</Text>
+
+    </View>
+    );
 };
 
 const styles = StyleSheet.create({});

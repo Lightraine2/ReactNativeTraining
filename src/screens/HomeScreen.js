1f401edbb13f6dcbda9c81f5b0191479a091abc2
Lightraine2 <laurencedale1986@gmail.com>
2020-07-05 11:19:19 +0100

    updated packages added box screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,39 +2,43 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
-  
+
   return <View>
-      <Text style={styles.text}>HomeScreen</Text>
-      <Button 
+    <Text style={styles.text}>HomeScreen</Text>
+    <Button
       onPress={() => {
         navigation.navigate('Components')
       }}
       title="Go to Components Demo"
-      />
-      <Button 
+    />
+    <Button
       title="Go to List Demo"
       onPress={() => navigation.navigate('List')}
-      />
-      <Button 
+    />
+    <Button
       title="Go to Image Demo"
       onPress={() => navigation.navigate('Image')}
-      />
-      <Button 
+    />
+    <Button
       title="Go to Counter (state) Demo"
       onPress={() => navigation.navigate('Counter')}
-      />
-      <Button 
+    />
+    <Button
       title="Go to Color (state 2) Demo"
       onPress={() => navigation.navigate('Color')}
-      />
-      <Button 
+    />
+    <Button
       title="Go to Square (state 3) Demo"
       onPress={() => navigation.navigate('Square')}
-      />
-      <Button 
+    />
+    <Button
       title="Go to Text Demo"
       onPress={() => navigation.navigate('Text')}
-      />
+    />
+    <Button
+      title="Go to Box Demo"
+      onPress={() => navigation.navigate('Box')}
+    />
   </View>
 };
 
